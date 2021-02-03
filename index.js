723712f590f27ac64eb11eb176fc74fb6611875e
zavedev <66022270+ZaveDev@users.noreply.github.com>
2021-02-03 14:59:59 -0500

    setup for chat feature

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const server = require('./server')
-const chalk = require('chalk')
+const app = require("./server");
+const socket = require("socket.io");
+const chalk = require("chalk");
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000
-
-server.listen(PORT, () => {
+const server = app.listen(PORT, () =>
   console.log(chalk.blue(`>> [Port ${PORT}] I'm listening...`))
-}) 
+);
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+io.on("connection", (socket) => {
+  console.log(socket.handshake.query);
+});
--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const app = express();
 
 const authRouter = require("./auth/auth-router");
 const client_growr_connectionRouter = require("./router/client_growr_connection");
 const userRouter = require("./router/user");
 const restricted = require("./auth/restricted-middleware");
 
-const server = express();
-
-server.use(helmet());
-server.use(express.json());
-server.use(
+app.use(helmet());
+app.use(express.json());
+app.use(
   cors({
     origin: "*",
     credentials: true,
   })
 );
 
-server.use("/auth", authRouter);
-server.use("/client_growr_connection", client_growr_connectionRouter);
-server.use("/user", userRouter);
+app.use("/auth", authRouter);
+app.use("/client_growr_connection", client_growr_connectionRouter);
+app.use("/user", userRouter);
 
-server.get("/", (req, res) => {
+app.get("/", (req, res) => {
+  res.sendFile("/Users/z/Documents/projects/Plantr Whole/plantrBE/index.html");
   res.status(200).json({ Victor_Frankenstein: "Its ALIVE" });
 });
 
-module.exports = server;
+module.exports = app;
